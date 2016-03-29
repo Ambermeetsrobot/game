5064fa91857a22c59687f88821c62c0ed0dc4a42
Amber Ward and Kendra Dunham <kendra.dunham@gmail.com>
2016-03-28 20:31:20 -0700

    add javascript sent from God

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,41 @@
-// business logic
-function Balance(balance) {
-  this.balance = balance;
+var dieRoll = (Math.floor(Math.random() * 6) + 1;
+
+function Player (roundscore, totalscore) {
+  this.roundscore = roundscore;
+  this.totalscore = totalscore;
 }
 
-BankAccount.prototype.deposit = function(newDeposit) {
-  this.balance = this.balance + this.deposit;
+Player.prototype.roll = function() {
+  var dieRoll = Math.floor(Math.random() * 6) + 1;
+  return dieRoll;
+};
+
+Player.prototype.turn = function() {
+  var value = this.roll();
+  if (value !== 1) {
+  this.roundscore = this.roundscore + value;
+    } else { this.roundscore = 0;
+    }
+};
+
+Player.prototype.hold = function() {
+  this.totalscore = this.totalscore + this.roundscore; 
 }
 
-BankAccount.prototype.withdraw = function(newWithdraw) {
-  this.balance = this.balance - this.withdraw;
+Roundscore.prototype.sum = function(dieRoll) {
+
 }
 
-// user interface
+
 $(document).ready(function() {
-  $("form#new-account").submit(function(event){
+  $("form#play").submit(function(event){
     event.preventDefault();
-    var accountName = $("input#new-name").val();
-    var newWithdraw = parseInt($("input#first-withdraw").val());
-    var newDeposit = parseInt($("input#new-deposit").val());
-    // var newBalance = new Balance();
-    
-    $("#accounthistory").append(newBalance)
 
-  });
-});
+    var roundScore = new
+
+
+
+Two standard dice are rolled. If neither shows a 1, their sum is added to the turn total.
+If a single 1 is rolled, the player scores nothing and the turn ends.
+If two 1s are rolled, the player’s entire score is lost, and the turn ends.
+If a double is rolled, the point total is added to the turn total as with any roll but the player is obligated to roll again (possible sub-variation of the two-dice game)
